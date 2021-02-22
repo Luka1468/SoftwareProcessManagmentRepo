5dbac2854afa455d75d5a1b45f6de771f8df9040
Luka Necajev <47037253+Luka1468@users.noreply.github.com>
2021-02-22 14:53:45 -0500

    Add files via upload

---
+++ ICE05_Test/src/ICE_5/LukaICE05.java
@@ -0,0 +1,12 @@
+package ICE_5;
+public class LukaICE05 {
+	
+	
+	
+	public int countTo10() {
+		for (int i=0; i <= 10; i++) {
+			System.out.print("Number is " + i);
+		}
+		return 10;
+	}
+}
---
+++ ICE05_Test/src/module-info.java
@@ -0,0 +1,2 @@
+module ICE05_Test {
+}
